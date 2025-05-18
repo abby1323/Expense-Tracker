595b1b83c4a0911019b84142ecbb8331ff117e1b
abhi1408-ch <abhi.chakraborty.2024@gmail.com>
2025-05-18 12:24:02 +0530

    initial commit

--- src/main/java/com/practice/ExpenseTracker/ExpensetrackerApplication.java
+++ src/main/java/com/practice/ExpenseTracker/ExpensetrackerApplication.java
@@ -1,4 +1,4 @@
-package com.practice.expensetracker;
+package com.practice.ExpenseTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
